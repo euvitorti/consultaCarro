3b73047aa65072e940a84e4d09615fc281d050e5
João Vítor Santos Lima <jvitor.ti03@gmail.com>
2024-05-04 15:37:00 -0300

    Nome da classe atualizado

--- assets/FipeTable/FipeTable/src/main/java/br/com/alura/FipeTable/service/ConvertData.java
+++ assets/FipeTable/FipeTable/src/main/java/br/com/alura/FipeTable/service/ConvertData.java
@@ -1,5 +1,33 @@
 package br.com.alura.FipeTable.service;
 
-public interface ConvertData {
-    <T> T getData(String json, Class<T> tClass);
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.type.CollectionType;
+
+import java.util.Collections;
+import java.util.List;
+
+public class ConvertData implements TConvertData {
+
+    private ObjectMapper objectMapper = new ObjectMapper();
+
+    @Override
+    public <T> T getData(String json, Class<T> tClass) {
+        try {
+            return objectMapper.readValue(json, tClass);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public <T> List<T> getList(String json, Class<T> tClass) {
+        CollectionType list = objectMapper.getTypeFactory()
+                .constructCollectionType(List.class, tClass);
+        try {
+            return objectMapper.readValue(json, list);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
